be5328bcba1adfd1f39562c3cfa475a38e130374
Deepak Mishra <deepak.code2436@gmail.com>
2022-05-21 19:22:00 +0530

    updadted

---
+++ main.js
@@ -0,0 +1,39 @@
+// var : GLOBAL SCOPE
+
+// let , const - ES6 FEATURES : BLOCK LEVEL SCOPE
+
+//Datatype :-
+// BIGINT, STRING, NUM, BOOLEAN, NULL, UNDEFINED, SYMBOL
+
+
+//let myNum = 20;
+//console.log(myNum);
+//console.log(typeof(myNum));
+
+
+// Looping statement:
+
+
+// if-else
+
+// var tomm ="rains";
+
+// if(tomm == "rain")
+// {
+//     console.log("raining");
+// }
+// else{
+//     console.log("not raining");
+// }
+
+//var age = 18;
+//console.log((age>=18)?"you can vote": "you cant vote");
+
+var age = 17;
+if (age>=18)
+{
+    console.log("you can vote");
+}
+else{
+    console.log("you cant vote");
+}
