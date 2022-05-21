47401909ae665222c27eb16c43cb3000ab6efd34
Deepak Mishra <deepak.code2436@gmail.com>
2022-05-21 19:44:27 +0530

    updadted

--- main.js
+++ main.js
@@ -29,11 +29,38 @@
 //var age = 18;
 //console.log((age>=18)?"you can vote": "you cant vote");
 
-var age = 17;
-if (age>=18)
-{
-    console.log("you can vote");
-}
-else{
-    console.log("you cant vote");
-}
+// var age = 17;
+// if (age>=18)
+// {
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("you cant vote");
+// }
+
+//Switch statement:
+
+// var area  = circle;
+// var PI = 3.14, l = 5, b = 4, r = 3;
+// switch (area){
+//     case "circle":
+//         console.log("the area of circle is :" + PI*r**2);
+//         Break;
+//         case 'triangle':
+//             console.log("the area of triangle is :"+(l*b)/2);
+//             Break;
+// }
+
+// WHILE LOOP:
+// var num = 4;
+// while (num <=10){
+//     console.log(num);
+//     num++;
+// }
+//DO-WHILE
+
+var num = 10;
+do{
+    console.log(num);
+    num++;
+}while(num<=12);
